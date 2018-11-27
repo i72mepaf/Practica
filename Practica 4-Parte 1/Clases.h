5ce7e0dabf672c33444354a10a0b1598160ef9d2
Mifsut12 <rickydelau17>
2018-11-27 10:38:06 +0100

    Subida Clase alumno

--- Practica 4-Parte 1/Clases.h
+++ Practica 4-Parte 1/Clases.h
@@ -1,2 +1,35 @@
-
-class Alumno
+using namespace std;
+class Alumno {
+private: 
+	string nombre_;
+	string apellidos_;
+	string DNI_;
+	string correo_;
+	int telefono_;
+	int direccion_;
+	int cursoMasAlto_;
+	string fechaNacimiento_;
+	bool ¿lider?_;
+	int grupo_;
+public:
+	string getNombre();
+	string getApellidos();
+	string getDNI();
+	string getCorreo();
+	int getTelefono();
+	int getDireccion();
+	int getCursoMasAlto();
+	string getFechaNacimiento();
+	int getGrupo();
+	bool getLider();
+	void setNombre();
+	void setApellidos();
+	void setDNI();
+	void setCorreo();
+	void setTelefono();
+	void setDireccion();
+	void setCursoMasAlto();
+	void setFechaNacimiento();
+	void setLider();
+	void setGrupo();
+}

0b68108ab49f1bc1a9511752a6d22f8918d3cc18
Anhe <angelbixde@hotmail.com>
2018-11-27 10:34:59 +0100

    ClaseBaseAlumnos

--- Practica 4-Parte 1/Clases.h
+++ Practica 4-Parte 1/Clases.h
@@ -1,2 +1,18 @@
+#include <list.h>
+#include <iostream>
+#include <string>
+
 
 class Alumno
+
+
+class BaseAlumnos{
+private:
+	list<Alumno> alumnos_;
+	void buscarAlumno();
+public:
+	void insertarAlumno();
+	void eliminarAlumno();
+	void mostrarAlumno();
+	void modificarAlumno();
+}

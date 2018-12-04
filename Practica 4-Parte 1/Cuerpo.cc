3ef6aa5ba61b315f0f735920ada513a806df9823
Felipe Mérida Palacios Mi PC <i72mepaf@uco.es>
2018-12-04 20:57:40 +0100

    IdentificarProfesor V1

--- Practica 4-Parte 1/Cuerpo.cc
+++ Practica 4-Parte 1/Cuerpo.cc
@@ -23,43 +23,17 @@ es un usuario no identificado. Puedo trollear a la aplicación diciendo que soy 
 cuando realmente no lo soy (Hay que comprobar esto de alguna forma.) Lo de vuelva al punto 4.1 
 se puede solucionar con un flujo loop. Las comprobaciones se pueden hacer en el mismo sistema, 
 donde se haga un ‘match’ entre las credenciales del fichero y las que mete el usuario.*/
-/*
-Cuando se deseen guardar registros de este tipo en un archivo, se puede hacer de la manera siguiente:
-
- Persona p1;
- Persona p2;
-
- strcpy( p1.nombre, "Baltasar" );
- strcpy( p1.telefono, "988387028" );
- p1.edad = 33;
-
- strcpy( p2.nombre, "Pedro" );
- strcpy( p2.telefono, "988387018" );
- p1.edad = 33;
-
- ofstream f( "datos.bin", ios::binary );
- if ( f.is_open() ) {
-	 f.write( (char *) &p1, sizeof( Persona ) );
-	 f.write( (char *) &p2, sizeof( Persona ) );
- } else cout << "Error de apertura de archivo." << endl;
-}
-
-Es posible realizar una lectura de cada registro de la siguiente forma:
-
- Persona p;
- ifstream f( "datos.bin", ios::binary );
-
- if ( f.is_open() ) {
-	f.read( (char *) &p, sizeof( Persona ) );
-	while( !f.eof() ) {
-		volcar( cout, p );
-		f.read( (char *) &p, sizeof( Persona ) );
-	}
- } else cout << "Error de apertura de archivo." << endl;
-}
-
-*/
+	int opcion = 0;
+	while(opcion!=1 && opcion!=2){
+	cout << "¿Es profesor Coordinador o Ayudante?" << endl;
+	cout << "1. Coordinador\n2. Ayudante" << endl;
+	cin >> opcion;
 	system("clear");
+	if(opcion!=1 && opcion!=2){
+		cout << "Número Incorrecto. Intentelo de nuevo" <<endl;
+	}
+	}
+
 	Profesor aux;
 	ifstream f("credenciales.bin", ios::binary);
 	if (f.is_open()){

04c3f11472e7e9d8bea16650784e0c3258d1f72c
Anhe <angelbixde@hotmail.com>
2018-12-04 10:49:06 +0100

    MostrarAlumno

--- Practica 4-Parte 1/Cuerpo.cc
+++ Practica 4-Parte 1/Cuerpo.cc
@@ -1,4 +1,13 @@
 #include "Clases.h"
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+bool sortByCursoa(Alumno &lhs,Alumno &rhs){ return lhs.getCurso() > rhs.getCurso() ; }
+bool sortByCursod(Alumno &lhs,Alumno &rhs){ return lhs.getCurso() < rhs.getCurso() ; }
+bool sortByApella(Alumno &lhs,Alumno &rhs){ return lhs.getApellidos() > rhs.getApellidos() ; }
+bool sortByApella(Alumno &lhs,Alumno &rhs){ return lhs.getApellidos() < rhs.getApellidos() ; }
 
 void BaseAlumnos::eliminarAlumno(){ //Felipe
 
@@ -8,4 +17,127 @@ bool Profesor::identificaProfesor(){ //Felipe
 }
 void Profesor::eliminarBaseAlumnos(){ //Felipe
 
+}
+
+
+void BaseAlumnos::mostrarAlumno(){
+	int menu,menu2;
+	int comp=0;
+	int comp2=0;
+	int vit=0;
+	vector <Alumno> v(150);
+	for(i=alumnos_.begin();i != alumnos_.end();i++){
+		v[vit].setNombre(i->getNombre());
+		v[vit].setApellidos(i->getApellidos());
+		v[vit].setDni(i->getDNI());
+		v[vit].setCorreo(i->getCorreo());
+		v[vit].setTelefono(i->getTelefono());
+		v[vit].setDireccion(i->getDireccion());
+		v[vit].setCursoMasAlto(i->getCurso());
+		v[vit].setFechaNacimiento(i->getFechaNacimiento());
+		v[vit].setGrupo(i->getGrupo());
+		v[vit].setLider(i->getLider());
+		vit++;
+	}
+	do{
+	cout >> "Mostrar todos los alumnos ....1"<< endl;
+	cout >> "Mostrar solo un alumno .......2"<< endl;
+	cin>> menu;
+	list <Alumno>:: iterator i;
+	//system("cls");
+	
+		switch(menu){
+			case 1:
+				do{
+				cout >> "Mostrar todos los alumnos ordenados ascendentemente por curso mas alto matriculado....1"<< endl;
+				cout >> "Mostrar todos los alumnos ordenados descendentemente por curso mas alto matriculado...2"<< endl;
+				cout >> "Mostrar todos los alumnos ordenados ascendentemente por apellidos.....................3"<< endl;
+				cout >> "Mostrar todos los alumnos ordenados descendentemente por apellidos....................4"<< endl;
+				cin>> menu2;			
+					switch(menu2){
+						case 1:
+						sort(v.begin(),v.end(),sortByCursoa);
+						imprimeVector(v);
+						comp2=1;
+						break;
+
+						case 2:
+						sort(v.begin(),v.end(),sortByCursod);
+						imprimeVector(v);
+						comp2=1;
+						break;
+
+						case 3:
+						comp2=1;
+						sort(v.begin(),v.end(),sortByApella);
+						imprimeVector(v);
+						break;
+
+						case 4:
+						sort(v.begin(),v.end(),sortByApelld);
+						imprimeVector(v);
+						comp2=1;
+						break;
+
+						default:
+							cout >> "Error ,introduzca una opcion válida"<< endl;
+							break;					
+					}
+				}while(comp2 == 0);
+
+				comp=1;
+				break;
+
+			case 2:
+				do{
+					cout >>"Mostrar un alumno por dni..........1"
+					cout >>"Mostrar un alumno por Apellidos....2"
+					cin >> menu2
+					switch(menu2){
+						case 1:
+						for (int i=0;i<v.size();i++){
+							if
+						}
+
+						comp2=1
+						break;
+
+						case 2:
+						comp2=1;
+						break;
+
+						default:
+							cout >> "Error ,introduzca una opcion válida"<< endl;
+							break; 
+					}
+
+				}while(comp2 == 0),
+
+
+
+
+
+				comp=1;
+				break;
+
+			default:
+				cout >> "Error ,introduzca una opcion válida"<< endl;
+				break;
+		}
+	}while(comp == 0);
+}
+
+void imprimeVector(vector <Alumno> v){
+	for(int i=0;i< v.size();i++){
+			cout<< v[i].getNombre()<<endl;
+			cout<< v[i].getApellidos()<<endl;
+			cout<< v[i].getDNI()<<endl;
+			cout<< v[i].getCorreo()<<endl;
+			cout<< v[i].getTelefono()<<endl;
+			cout<< v[i].getDireccion()<<endl;
+			cout<< v[i].getCursoMasAlto()<<endl;
+			cout<< v[i].getFechaNacimiento()<<endl;
+			cout<< v[i].getGrupo()<<endl;
+			cout<< v[i].getLider()<<endl;
+	}
 }

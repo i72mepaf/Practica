0ea2d7fcb8597638621de5198a3c31b705b854e3
Felipe Mérida Palacios Mi PC <i72mepaf@uco.es>
2018-12-07 16:22:23 +0100

    libreria std e identificar profesor (este ultimo lo hemos añadido entero ya que por lo complejo que era hemos tenido que desarrollarlo, editarlo y probarlo en un fichero aparte

--- Practica 4-Parte 1/Cuerpo.cc
+++ Practica 4-Parte 1/Cuerpo.cc
@@ -5,6 +5,7 @@
 #include <list>
 #include <string>
 #include <vector>
+using namespace std;
 
 bool sortByCursoa(Alumno &lhs,Alumno &rhs){ return lhs.getCurso() > rhs.getCurso() ; }
 bool sortByCursod(Alumno &lhs,Alumno &rhs){ return lhs.getCurso() < rhs.getCurso() ; }
@@ -15,36 +16,180 @@ bool sortByApella(Alumno &lhs,Alumno &rhs){ return lhs.getApellidos() < rhs.getA
 void BaseAlumnos::eliminarAlumno(){ //Felipe
 
 }
-bool Profesor::identificaProfesor(){ /*Felipe //Requisito no funcional (el archivo de credenciales tiene 
-que haber profesores, mínimo el profesor coordinador)
-
-- SecuenciaIdentificarProfesor.jpg: El actor que hace este flujo de acciones no es el profesor, 
-es un usuario no identificado. Puedo trollear a la aplicación diciendo que soy coordinador 
-cuando realmente no lo soy (Hay que comprobar esto de alguna forma.) Lo de vuelva al punto 4.1 
-se puede solucionar con un flujo loop. Las comprobaciones se pueden hacer en el mismo sistema, 
-donde se haga un ‘match’ entre las credenciales del fichero y las que mete el usuario.*/
-	int opcion = 0;
-	while(opcion!=1 && opcion!=2){
-	cout << "¿Es profesor Coordinador o Ayudante?" << endl;
-	cout << "1. Coordinador\n2. Ayudante" << endl;
-	cin >> opcion;
-	system("clear");
-	if(opcion!=1 && opcion!=2){
-		cout << "Número Incorrecto. Intentelo de nuevo" <<endl;
-	}
-	}
-
-	Profesor aux;
-	ifstream f("credenciales.bin", ios::binary);
-	if (f.is_open()){
-		while(!f.eof()){
-			f.read((char*) &aux, sizeof(Profesor));
-
-
+struct Auxiliar
+{
+	char d[100];
+	char r[100];
+};
+bool Profesor::identificaProfesor(){
+	int opcion = 0, contadorTipo=0;
+	string dni, compDNI, compROL;
+	Auxiliar aux;
+	vector<Auxiliar> v;
+	ifstream fichero("credenciales.bin", ios::in | ios::binary);
+	if (fichero.is_open()){
+		while(!fichero.eof() && fichero.read((char *)&aux, sizeof(Auxiliar))){
+			v.push_back(aux);
 		}
-
 	}
-
+	else{
+		cout << "Error al abrir credenciales.bin porque no existe en el directorio." << endl;
+		exit(-1);
+	}
+   	fichero.close();
+		cout << "Bienvenido al programa de la base de datos de alumnos." << endl;
+	while(opcion!=1 && opcion!=2){
+		cout << "¿Es profesor Coordinador o Ayudante?" << endl;
+		cout << "1. Coordinador.\n2. Ayudante." << endl;
+		cin >> opcion;
+		system("clear");
+		if(opcion!=1 && opcion!=2){
+			cout << "Número Incorrecto. Intentelo de nuevo." << endl;
+		}
+		else{
+			if(opcion==1){//Coordinador
+				cout << "A continuación procederemos a identificarle como profesor Coordinador." << endl;
+				cout << "Introduzca DNI: ";
+				cin >> dni;
+				system("clear");
+   				for (int i = 0; i < v.size(); ++i)
+   				{
+   					compDNI = v[i].d;
+   					compROL = v[i].r;
+   					if(compDNI == dni && compROL == "Coordinador")
+   						cout << "Identificado Correctamente." << endl;
+   					while(compDNI == dni && compROL == "Coordinador"){
+   						cout << "¿Desea iniciar o cambiar DNI?\n1. Iniciar.\n2. Cambiar DNI." << endl;
+   						cin >> opcion;
+   						system("clear");
+   						if(opcion!= 1 && opcion!=2){
+   							cout << "Numero Incorrecto. Intentalo de nuevo." << endl;
+   						}
+   						else{
+   							if(opcion == 1){
+   								remove("credenciales.bin");
+   								ofstream fCreacion("credenciales.bin", ios::out | ios::binary);
+								if (fCreacion.is_open()){
+									for(int j=0; j < v.size(); j++)
+									fCreacion.write((char *)&v[j], sizeof(Auxiliar));
+   								}
+   								else{
+   									cout << "Error al abrir el fichero." << endl;
+   									exit(-1);
+   								}
+   								fCreacion.close();
+   								cout << "Iniciando..." << endl;
+   								setDni(compDNI);
+   								setRol(compROL);
+   								return true;
+   							}
+   							else{
+   								cout << "Introduzca el nuevo DNI: ";
+   								cin >> dni;
+   								remove("credenciales.bin");
+   								ofstream fEscCoord("credenciales.bin", ios::out | ios::binary);
+								if (fEscCoord.is_open()){
+									for(int j=0; j < v.size(); j++){
+										compROL = v[j].r;
+										if(compROL == "Coordinador")
+											strcpy(v[j].d, dni.c_str());
+										fEscCoord.write((char *)&v[j], sizeof(Auxiliar));
+									}
+   								}
+   								else{
+   									cout << "Error al abrir el fichero." << endl;
+   									exit(-1);
+   								}
+   								fEscCoord.close();
+   								cout << "DNI cambiado con exito. Iniciando..." << endl;
+   								setDni(dni);
+   								setRol("Coordinador");
+   								return true;
+   							}
+   						}
+   					}
+				}
+				cout << "Lo sentimos, no hemos podido identificarle, intentelo de nuevo." << endl;
+   				opcion = 3;
+   			}
+			else{//Ayudante
+				cout << "¿Desea registrarse o identificarse?" << endl;
+				cout << "1. Registrar.\n2. Identificar." << endl;
+				cin >> opcion;
+				system("clear");
+   				if(opcion!= 1 && opcion!=2){
+  					cout << "Numero Incorrecto. Intentalo de nuevo." << endl;
+  					opcion = 3;
+				}
+				else{
+					if(opcion == 1){//Registrarse
+						for (int i = 0; i < v.size(); ++i)
+						{
+							compROL = v[i].r;
+							if(compROL=="Ayudante")
+								contadorTipo++;
+						}
+						if(contadorTipo < 5){
+							cout << "Registro de un profesor ayudante.\nIntroduce el DNI: ";
+							cin >> dni;
+							strcpy(aux.d, dni.c_str());
+							strcpy(aux.r, "Ayudante");
+							v.push_back(aux);
+							remove("credenciales.bin");
+							ofstream fEscAyud("credenciales.bin", ios::out | ios::binary);
+							if (fEscAyud.is_open()){
+								for(int i=0; i < v.size(); i++)
+									fEscAyud.write((char *)&v[i], sizeof(Auxiliar));
+   							}
+   							else{
+   								cout << "Error al abrir el fichero." << endl;
+   								exit(-1);
+   							}
+   							fEscAyud.close();
+   							cout << "Registro correcto. Iniciando..." << endl;
+   							setDni(dni);
+   							setRol("Ayudante");
+   							return false;
+						}
+						else{
+							cout << "Lo sentimos. Ya existen registrados 5 profesores ayudantes." << endl;
+  							opcion = 3;
+						}
+					}
+					else{//Identificarse
+						cout << "A continuación procederemos a identificarle como profesor Ayudante." << endl;
+						cout << "Introduzca el DNI: ";
+						cin >> dni;
+						system("clear");
+   						for (int i = 0; i < v.size(); ++i)
+   						{
+   							compDNI = v[i].d;
+   							compROL = v[i].r;
+   							if(compDNI == dni && compROL == "Ayudante"){
+   								cout << "Identificado Correctamente. Iniciando..." << endl;
+   								remove("credenciales.bin");
+   								ofstream fCreacionA("credenciales.bin", ios::out | ios::binary);
+								if (fCreacionA.is_open()){
+									for(int j=0; j < v.size(); j++)
+									fCreacionA.write((char *)&v[j], sizeof(Auxiliar));
+   								}
+   								else{
+   									cout << "Error al abrir el fichero" << endl;
+   									exit(-1);
+   								}
+   								fCreacionA.close();
+   								setDni(compDNI);
+   								setRol(compROL);
+   								return false;
+							}
+						}
+							cout << "No se ha podido identificar, intentelo de nuevo." << endl;
+							opcion = 3;
+					}
+				}
+			}
+		}
+	}
 }
 void Profesor::eliminarBaseAlumnos(){ //Felipe
 
@@ -54,10 +199,10 @@ void Profesor::eliminarBaseAlumnos(){ //Felipe
 void BaseAlumnos::insertarAlumno(){
 	list<Alumno>::iterator it;
 	Alumno alumno;
-	std::string nombre;
-	std::string apellidos;
-	std::string DNI;
-	std::string correo;
+	string nombre;
+	string apellidos;
+	string DNI;
+	string correo;
 	int telefono;
 	int direccion
 	int cursoMasAlto;
@@ -66,42 +211,42 @@ void BaseAlumnos::insertarAlumno(){
 	int grupo;
 
 	
-	std::cout<<"Introduce el DNI del alumno"<<endl;
-	std::cin>>DNI;
+	cout<<"Introduce el DNI del alumno"<<endl;
+	cin>>DNI;
 	
 	it = alumnos_.begin();
 	while(it != alumnos_.end){
 		if(it.getDNI()==DNI) {
-			std::cout<<("ERROR.Este Alumno ya existe")<<endl;
+			cout<<("ERROR.Este Alumno ya existe")<<endl;
 			exit(-1);
 		}
 	}
-	std::cout<<"Introduzca el nombre del alumno"<<endl;
-	std::cin>>nombre;
-	std::cout>>"Introduce el apellido del alumno"<<endl;
-	std::cin>>apellidos;
-	std::cout<<"Introduce el corre del alumno"<<endl;
-	std::cin>>correo;
-	std::cout<<"Introduce el numero de telefono del alumno"<<endl;
-	std::cin>>telefono;
-	std::cout<<"Introduce la direccion del alumno"<<endl;
-	std::cin>>direccion;
-	std::cout<<"Introduce el curso mas alto en el que el alumno esta matriculado"<<endl;
-	std::cin>>cursoMasAlto;
-	std::cout<<"Introduce el dia en el que el alumno nacio"<<endl;
-	std::cin>>diaNacimiento;
-	std::cout<<"Introduce el mes de nacimiento del alumno"<<endl;
-	std::cin>>mesNacimiento;
-	std::cout<<"Introduce el año de nacimiento del alumno"<<endl;
-	std::cin>>anoNacimiento;
-	std::cout<<"Este alumno es lider de algun grupo"<<endl;
-	std::cin>>lider;
-	std::cout<<"Grupo del alumno"<<endl:
+	cout<<"Introduzca el nombre del alumno"<<endl;
+	cin>>nombre;
+	cout>>"Introduce el apellido del alumno"<<endl;
+	cin>>apellidos;
+	cout<<"Introduce el corre del alumno"<<endl;
+	cin>>correo;
+	cout<<"Introduce el numero de telefono del alumno"<<endl;
+	cin>>telefono;
+	cout<<"Introduce la direccion del alumno"<<endl;
+	cin>>direccion;
+	cout<<"Introduce el curso mas alto en el que el alumno esta matriculado"<<endl;
+	cin>>cursoMasAlto;
+	cout<<"Introduce el dia en el que el alumno nacio"<<endl;
+	cin>>diaNacimiento;
+	cout<<"Introduce el mes de nacimiento del alumno"<<endl;
+	cin>>mesNacimiento;
+	cout<<"Introduce el año de nacimiento del alumno"<<endl;
+	cin>>anoNacimiento;
+	cout<<"Este alumno es lider de algun grupo"<<endl;
+	cin>>lider;
+	cout<<"Grupo del alumno"<<endl:
 
-	alumno.setNombre(std::string nombre);
-	alumno.setApellidos(std::string apellidos);
-	alumno.setDNI(std::string DNI);
-	alumno.setCorreo(std::string correo);
+	alumno.setNombre(string nombre);
+	alumno.setApellidos(string apellidos);
+	alumno.setDNI(string DNI);
+	alumno.setCorreo(string correo);
 	alumno.setTelefono(int telefono);
 	alumno.setDireccion(int direccion);
 	alumno.setCursoMasAlto(int cursoMasAlto);
@@ -111,16 +256,16 @@ void BaseAlumnos::insertarAlumno(){
 	alumnos_.push_back(alumno);
 }
 
-void Alumno::setNombre(std::string nombre) {
+void Alumno::setNombre(string nombre) {
 	nombre_=nombre;
 }
-void Alumno::setApellidos(std::string apellidos) {
+void Alumno::setApellidos(string apellidos) {
 	apellidos_=apellidos;
 }
-void Alumno::setDNI(std::string DNI) {
+void Alumno::setDNI(string DNI) {
 	DNI_ = DNI;
 }
-void Alumno::setCorreo(std::string correo) {
+void Alumno::setCorreo(string correo) {
 	correo_ = correo;
 }
 void Alumno::setTelefono(int telefono) {
@@ -133,80 +278,80 @@ void Alumno::setCursoMasAlto(int cursoMasAlto) {
 	cursoMasAlto_ = cursoMasAlto
 }
 void Alumno::setFechaNacimiento(int diaNacimiento, int mesNacimiento, int anoNacimiento) {
-	std::string aux;
+	string aux;
 	if(mesNacimiento <= 0 || mesNacimiento > 12) {
-		std::cout<<"El mes numero "<<mesNacimiento<<"no existe"<<endl;
+		cout<<"El mes numero "<<mesNacimiento<<"no existe"<<endl;
 		exit(-1);
 	}
 	if(mesNacimiento == 1) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 2) {
 		if(diaNacimiento <= 0 || diaNacimiento > 28) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 3) {
 		if(diaNacimiento <= 0 || diaNacimiento > 29) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 4) {
 		if(diaNacimiento <= 0 || diaNacimiento > 30) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 5) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 6) {
 		if(diaNacimiento <= 0 || diaNacimiento > 30) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 7) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 8) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 9) {
 		if(diaNacimiento <= 0 || diaNacimiento > 30) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 10) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 11) {
 		if(diaNacimiento <= 0 || diaNacimiento > 30) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
 	if(mesNacimiento == 12) {
 		if(diaNacimiento <= 0 || diaNacimiento > 31) {
-			std::cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
+			cout<<"El dia "<<diaNacimiento<<"no esta en el mes "<<mesNacimiento<<endl;
 			exit(-1);
 		}
 	}
